32b56172b6d59e2548305c587878e33747bac39d
matao1984 <matao1984@hotmail.com>
2025-10-27 20:26:20 -0400

    Update dates

--- TemCompanion/TemCompanion.py
+++ TemCompanion/TemCompanion.py
@@ -161,7 +161,7 @@ from skimage.transform import warp, rescale, resize
 
 
 ver = '1.3'
-rdate = '2025-10-25'
+rdate = '2025-10-27'
 
 #===================Import internal modules==========================================
 from GPA import GPA, norm_img, create_mask, refine_center

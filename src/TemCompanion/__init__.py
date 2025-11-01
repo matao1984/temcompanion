8ce2db5df633e605763912193e0f58188c263e00
matao1984 <matao1984@hotmail.com>
2025-11-01 19:16:08 -0400

    Created

--- src/TemCompanion/__init__.py
+++
@@ -1,9 +0,0 @@
-"""TemCompanion package initializer.
-
-Ensures this directory is recognized as a Python package so that
-absolute imports like `TemCompanion.main` work in all environments
-(dev, python -m, and PyInstaller bundles).
-"""
-
-# Optional package metadata
-__all__ = []

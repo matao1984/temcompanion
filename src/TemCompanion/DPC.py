39d1e12a38363f18d067c888e6d16cd970a05c25
matao1984 <matao1984@hotmail.com>
2025-11-04 18:43:45 -0500

    Support non square images in reconstruction Use filters.gauussian_lowpass for high pass filtering

--- src/TemCompanion/DPC.py
+++ src/TemCompanion/DPC.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.fft import fft2, fftshift, ifft2, ifftshift, fftfreq
+from .filters import gaussian_lowpass, pad_to_square
 # Integrate DPC function
 def reconstruct_iDPC(DPCx, DPCy, rotation=0, cutoff=0.02):
     '''
@@ -11,23 +12,32 @@ def reconstruct_iDPC(DPCx, DPCy, rotation=0, cutoff=0.02):
     '''
     if DPCx.ndim == 2:
         im_y, im_x = DPCx.shape
+        if im_x != im_y:
+            DPCx = pad_to_square(DPCx)
+            DPCy = pad_to_square(DPCy)
         # Rotate the DPC vector images
         if rotation != 0:
             ang = rotation * np.pi / 180 # Convert to radian
             DPCx, DPCy = rotate_vector(DPCx, DPCy, ang)
         # Make the k grid
-        qx, qy = fftfreq(im_x), fftfreq(im_y)
+        qx, qy = fftfreq(DPCx.shape[1]), fftfreq(DPCx.shape[0])
         kx, ky = np.meshgrid(qx, qy)
         d1 = kx * fft2(DPCx) + ky * fft2(DPCy)
         k2 = kx**2 + ky**2
         d2 = 2 * np.pi * k2 * 1j
         d2[0,0] = np.inf
         iDPC_fft = d1 / d2
+        iDPC_fftshift = fftshift(iDPC_fft)
+
         # Apply a Gaussian high pass filter
-        g = gaussian_high_pass((im_y, im_x), cutoff)
-        iDPC_fft = iDPC_fft * g
-        iDPC = np.real(ifft2(iDPC_fft))
-        #iDPC -= np.min(iDPC)
+        if cutoff > 0 and cutoff <= 1:
+            iDPC = gaussian_lowpass(iDPC_fftshift, cutoff_ratio=1, hp_cutoff_ratio=cutoff, space='fourier')
+        else:
+            iDPC = np.real(ifft2(fftshift(iDPC_fftshift)))
+        
+        iDPC = iDPC[:im_y, :im_x]  # Crop back to original size if padded
+        
+        
     elif DPCx.ndim == 3:
         # Calculate on an image stack
         im_z = DPCx.shape[0]
@@ -37,19 +47,19 @@ def reconstruct_iDPC(DPCx, DPCy, rotation=0, cutoff=0.02):
     #iDPC_int16 = iDPC.astype('int16')
     return iDPC
     
-def gaussian_high_pass(shape, cutoff=0.02):
-    # Return a Gaussian high pass filter
-    im_y, im_x = shape
-    cx, cy = im_x // 2, im_y // 2
-    X, Y = np.meshgrid(np.arange(im_x), np.arange(im_y))
-    X -= cx
-    Y -= cy
-    sigma = im_x * cutoff
-    gaussian = np.exp(-(X**2 + Y**2) / (2 * sigma ** 2))
-    return ifftshift(1 - gaussian)
+# def gaussian_high_pass(shape, cutoff=0.02):
+#     # Return a Gaussian high pass filter
+#     im_y, im_x = shape
+#     cx, cy = im_x // 2, im_y // 2
+#     X, Y = np.meshgrid(np.arange(im_x), np.arange(im_y))
+#     X -= cx
+#     Y -= cy
+#     sigma = im_x * cutoff
+#     gaussian = np.exp(-(X**2 + Y**2) / (2 * sigma ** 2))
+#     return ifftshift(1 - gaussian)
 
 # Calculate the divergence
-def reconstruct_dDPC(DPCx, DPCy, rotation, inverse = False):
+def reconstruct_dDPC(DPCx, DPCy, rotation, cutoff=0.02, inverse=False):
     if DPCx.ndim == 2:
         # Rotate the DPC vector images
         if rotation != 0:
@@ -58,8 +68,11 @@ def reconstruct_dDPC(DPCx, DPCy, rotation, inverse = False):
         dDPCx = np.gradient(DPCx, axis=1)
         dDPCy = np.gradient(DPCy, axis=0)
         dDPC = dDPCx + dDPCy
+        if cutoff > 0 and cutoff < 1:
+            dDPC = gaussian_lowpass(dDPC, cutoff_ratio=1, hp_cutoff_ratio=cutoff)
         if inverse:
             dDPC = -dDPC
+
         #dDPC -= np.min(dDPC)
     elif DPCx.ndim == 3:
         im_z = DPCx.shape[0]
